af6ae64bc5427f25be9b8feace38b9419038fbd0
Malgorzata Kolakowska <m.a.kolakowska@googlemail.com>
2020-03-27 11:18:10 +0000

    Correct syntax error in processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -175,12 +175,12 @@
           /* START IF: if option is selected and option is not default */
           if (optionSelected && !option.default) {
             /* add price of option to variable price */
-            price[option.price] = 1;
+            price = price + option.price;
             /* END IF: if option is selected and option is not default */
             /* START ELSE IF: if option is not selected and option is default */
           } else if (!optionSelected && option.default) {
             /* deduct price of option from price */
-            price[option.price]--;
+            price = price - option.price;
             /* END ELSE IF: if option is not selected and option is default */
           }
           /* END LOOP: for each optionId in param.options */
@@ -188,7 +188,7 @@
         /* END LOOP: for each paramId in thisProduct.data.params */
       }
       /* set the contents of thisProduct.priceElem to be the value of variable price */
-      thisProduct.priceElem = price;
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
