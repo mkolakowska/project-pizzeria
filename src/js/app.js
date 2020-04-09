39a36f82d2d66919d8da00f63fee3d887b56c34f
Malgorzata Kolakowska <m.a.kolakowska@googlemail.com>
2020-04-09 18:21:24 +0100

    Add widget to booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,69 @@
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
-import { select, settings } from './settings.js';
+import { select, settings, classNames } from './settings.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thisApp.activePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = `#/${id}`;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    /* add class "active" to matching links, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == `#${pageId}`
+      );
+    }
+  },
+
+  initBooking: function () {
+    const thisApp = this;
+
+    thisApp.bookingContainer = document.querySelector(
+      select.containerOf.booking
+    );
+
+    thisApp.booking = new Booking(thisApp.bookingContainer);
+  },
+
   initMenu: function () {
     const thisApp = this;
 
@@ -12,10 +73,6 @@ const app = {
         thisApp.data.products[productData]
       );
     }
-    /*
-      const testProduct = new Product();
-      //// console.log('testProduct:', testProduct);
-      */
   },
 
   initData: function () {
@@ -30,14 +87,11 @@ const app = {
         return rawResponse.json();
       })
       .then(function (parsedResponse) {
-        console.log('parsedResponse', parsedResponse);
-
         /* save parsedResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
         /* execute initMenu method */
         thisApp.initMenu();
       });
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
   initCart: function () {
@@ -60,9 +114,10 @@ const app = {
     //// console.log('classNames:', classNames);
     //// console.log('settings:', settings);
     //// console.log('templates:', templates);
-
+    thisApp.initPages();
     thisApp.initCart();
     thisApp.initData();
+    thisApp.initBooking();
   },
 };
 app.init();
